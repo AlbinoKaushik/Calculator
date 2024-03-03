f6ca68151916c15b6bac0978f9b69e29b492d452
AlbinoKaushik <albinokaushik6618@gmail.com>
2024-03-03 10:23:10 +0530

    readme_added

---
+++ README.md
@@ -0,0 +1,6 @@
+# Calculator
+
+in working process 
+Author - Albino Kaushik
+<br>
+Dated - 03-03-2024
---
+++ script.js
@@ -0,0 +1,5 @@
+let btns = document.querySelectorAll("button");
+let Ans = document.querySelector("#head");
+let body = document.querySelector("#body");
+
+
