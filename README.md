9f40214f6165dace07379db22a4be254a5c15365
AlbinoKaushik <albinokaushik6618@gmail.com>
2024-03-03 10:34:50 +0530

    button works

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Calculator
 
 in working process 
+<br>
 Author - Albino Kaushik
 <br>
 Dated - 03-03-2024
--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let btns = document.querySelectorAll("button");
-let Ans = document.querySelector("#head");
+let head = document.querySelector("#head");
 let body = document.querySelector("#body");
 
 
+btns.forEach((btn)=>{
+  btn.addEventListener("click",()=>{
+    let value = btn.innerText;
+    console.log(value);
+
+  });
+
+});
